00b37cf0d3e3074b43ea4cc29261998274446dc5
Tom Fitzhenry <git@tom-fitzhenry.me.uk>
2014-12-27 10:03:26 +0000

    Create stub HTTP methods
    working offline!

--- src/reddit_nntp/main.clj
+++ src/reddit_nntp/main.clj
@@ -7,10 +7,24 @@
   (map (fn [child] (get child "data"))
        (get-in posts-repr ["data" "children"]))))
 
+(comment
 (defn grab-posts-from-reddit []
   (let [opts {:client-params { "http.useragent" "reddit-nntp" }}
         resp (client/get "http://www.reddit.com/r/crypto/.json" opts)]
     (get resp :body)))
+)
+
+(defn grab-posts-from-reddit []
+  (json/write-str
+   { "data"
+     { "children"
+       [
+        { "data"
+          { "id" "2qha"
+            "title" "Foo"}}
+        { "data"
+          { "id" "3ab4"
+            "title" "Bar"}}]}}))
 
 (defn pretty-print-post [post]
   (select-keys post ["title" "id"]))

cea55c20b802310964ce4f48675d7f1922232550
Tom Fitzhenry <git@tom-fitzhenry.me.uk>
2014-12-28 13:27:51 +0000

    Remove superfluous syntax

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.6.0"],
-    [org.clojure/data.json "0.2.5"],
+    [org.clojure/clojure "1.6.0"]
+    [org.clojure/data.json "0.2.5"]
     [clj-http "1.0.1"]]
   :main reddit-nntp.main)
